a4a5dab9860fbb6693ea52777aeab888bd7057ed
Bruno <sumar.bruno@yahoo.com>
2021-06-29 18:42:59 -0300

    teste

--- api/routes/front.js
+++ api/routes/front.js
@@ -1,6 +1,7 @@
 const frontOpts = {
     handler: (req, reply) => {
-        return reply.sendFile('index.html')
+        // return reply.sendFile('index.html')
+        return reply.send({a: 234234})
     },
 }
 
