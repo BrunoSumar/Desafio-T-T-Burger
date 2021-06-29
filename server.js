42379604f12d3094e8c2e66d9b78920a21d910dd
Bruno <sumar.bruno@yahoo.com>
2021-06-29 19:33:02 -0300

    teste heroku 4

---
+++ server.js
@@ -0,0 +1,24 @@
+const fastify = require('fastify')()
+// const fastify = require('fastify')({ logger: true })
+const path = require('path')
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, './front/dist'),
+})
+
+fastify.register(require('./routes/front'))
+fastify.register(require('./routes/api'))
+
+const PORT = process.env.PORT || 8080
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT)
+    console.log(`App is running on port ${ PORT }`);
+  } catch (error) {
+    fastify.log.error(error)
+    process.exit(1)
+  }
+}
+
+start()
