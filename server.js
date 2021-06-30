d2068bc620b40b70ee24d43bd5aae6d065052b88
Bruno <sumar.bruno@yahoo.com>
2021-06-29 21:04:40 -0300

    7

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8080
 
 const start = async () => {
   try {
-    await fastify.listen(PORT)
+    await fastify.listen(PORT, '0.0.0.0')
     console.log(`App is running on port ${ PORT }`);
   } catch (error) {
     fastify.log.error(error)
